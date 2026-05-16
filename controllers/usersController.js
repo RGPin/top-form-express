df3e51b598100995b67284126cb6af642e805388
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-16 14:05:56 +0800

    validator middleware in controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,23 @@
 const usersStorage = require("../storages/usersStorage");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First Name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First Name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last Name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last Name ${lengthErr}`),
+];
 
 exports.usersListGet = (req, res) => {
   res.render("index", {
@@ -11,8 +30,18 @@ exports.usersCreateGet = (req, res) => {
   res.render("createUser", { title: "Create user" });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
-  res.redirect("/");
-};
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
