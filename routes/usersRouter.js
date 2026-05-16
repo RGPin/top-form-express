5721ac817452717c9d6ecbdd126e225cd14179f5
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-16 13:48:30 +0800

    Add controllers to handle creating and storing users, create storage class to simulate db, add views, render views from routers

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const usersController = require("../controllers/usersController");
 const usersRouter = express.Router();
 
-usersRouter.get("/", (req, res) => res.send("Hallo warudo"));
+usersRouter.get("/", usersController.usersListGet);
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
 
 module.exports = usersRouter;
